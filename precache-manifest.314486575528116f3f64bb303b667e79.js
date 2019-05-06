3e20de7f17e8097bf580713ce41c4f439328160c
Jake Goldsborough <jake.goldsborough@pm.me>
2019-05-06 15:29:54 -0700

    Updates

---
+++ precache-manifest.314486575528116f3f64bb303b667e79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88725865ccf2550c346a39b08f3aa8eb",
+    "url": "/bag-builder/index.html"
+  },
+  {
+    "revision": "b191a2bd61e6e454dc33",
+    "url": "/bag-builder/static/css/main.90b4c065.chunk.css"
+  },
+  {
+    "revision": "f618ec5a390b72f64f00",
+    "url": "/bag-builder/static/js/2.f477aa32.chunk.js"
+  },
+  {
+    "revision": "b191a2bd61e6e454dc33",
+    "url": "/bag-builder/static/js/main.15ec4eb0.chunk.js"
+  },
+  {
+    "revision": "a47f32ae8eb8f4d2c66e",
+    "url": "/bag-builder/static/js/runtime~main.78b9a7c6.js"
+  }
+]);
